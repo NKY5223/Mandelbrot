808c1c2a5038bcbed0c307e65d61b61dc99107ce
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2022-10-28 21:10:41 +0800

    AAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEE 2
    AAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAAAAAAAAAAAAÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆÆEEEEEEEEEEEEEEEEEEEEEEEEEE

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function render() {
     let w = settings.right - settings.left;
     let h = settings.bottom - settings.top;
     const data = [];
-    for (let y = 0; y < settings.resy; y++) for (let x = 0; x < settings.resx; x++) data.push(...palette(mandelbrot(settings.left + w * x / settings.resx, settings.top + h * y / settings.resy)), 1);
+    for (let y = 0; y < settings.resy; y++) for (let x = 0; x < settings.resx; x++) data.push(...settings.palette(mandelbrot(settings.left + w * x / settings.resx, settings.top + h * y / settings.resy)), 1);
     ctx.putImageData(new ImageData(data, settings.resx, settings.resy), 0, 0);
 }
 function mandelbrot(cr, ci) {
